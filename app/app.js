05a2574518cb935d297150d48a4ef8c9286a6332
tizot <masset.camille@gmail.com>
2014-10-03 19:29:25 +0200

    Adds a categories variable in the whole app

--- app/app.js
+++ app/app.js
@@ -15,11 +15,34 @@ var ergoApp = angular.module('ergo.app', [
 ergoApp.config(['$stateProvider', '$urlRouterProvider', 
     function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
+        var categories = [
+            {code: '8-m-U', label: 'Huit masculin universitaire'},
+            {code: '8-f-U', label: 'Huit féminin universitaire'},
+            {code: '4-m-U', label: 'Quatre masculin universitaire'},
+            {code: '4-f-U', label: 'Quatre féminin universitaire'},
+            {code: '4-mix-U', label: 'Quatre mixte universitaire'},
+            {code: '2-m-U', label: 'Double masculin universitaire'},
+            {code: '2-f-U', label: 'Double féminin universitaire'},
+            {code: '1-m-U', label: 'Individuel masculin universitaire'},
+            {code: '1-f-U', label: 'Individuel féminin universitaire'},
+            {code: '8-m-S', label: 'Huit masculin senior'},
+            {code: '8-f-S', label: 'Huit féminin senior'},
+            {code: '4-m-S', label: 'Quatre masculin senior'},
+            {code: '4-f-S', label: 'Quatre féminin senior'},
+            {code: '4-mix-S', label: 'Quatre mixte senior'},
+            {code: '2-m-S', label: 'Double masculin senior'},
+            {code: '2-f-S', label: 'Double féminin senior'},
+            {code: '1-m-S', label: 'Individuel masculin senior'},
+            {code: '1-f-S', label: 'Individuel féminin senior'}
+        ];
         
         $stateProvider
         // partie publique
         .state('root', {
-            templateUrl: 'views/layout.html'
+            templateUrl: 'views/layout.html',
+            controller: function($rootScope) {
+                $rootScope.categories = categories;
+            }
         })
         .state('root.presentation', {
             url: '/',
@@ -55,7 +78,10 @@ ergoApp.config(['$stateProvider', '$urlRouterProvider',
         // partie admin
         .state('admin', {
             templateUrl: 'views/admin.html',
-            url: '/admin'
+            url: '/admin',
+            controller: function($rootScope) {
+                $rootScope.categories = categories;
+            }
         })
         .state('admin.dashboard', {
             url: '/',
